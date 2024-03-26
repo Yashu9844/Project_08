f9ff2a94c93f0d58ff8409f0acafd8ffdb570c07
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-03-27 00:01:09 +0530

    Nav created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
+import Page1 from './Components/Page1'
 
 const App = () => {
   return (
     <div className='w-full h-screen bg-zinc-900 text-white'>
        <NavBar/>
+       <Page1/>
     </div>
   )
 }
---
+++ src/Components/Page1.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Page1 = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-900'>
+      <div className="w-1/2 h-[70%] bg-red-500 flex flex-col px-7 py-5">
+        <div className="text-[8vw] font-bold tracking-tight font-['Founders Grotesk']"><h1>WE CREATE</h1></div>
+        <div className="">
+            <img src="" alt="" />
+            <h1>EYE_OPENING</h1>
+        </div>
+        <div className=" ">
+            <h1>PRESENTATIONS</h1>
+        </div>
+        
+      </div>
+      <div className="">hello</div>
+    </div>
+  )
+}
+
+export default Page1
